f5d5fa6f9dcf62dc770e0f15f0ded50d49dea84f
Mario <mpariciob@gmail.com>
2023-05-05 22:11:36 +0200

    Ya funciona la lógica de login :)

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,61 +1,64 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
-import {Link} from 'react-router-dom'
+import { auth } from "../firebase";
+import { Link } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate(); // Get access to the navigate function
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // Redirect the user to the desired route after successful login
+      navigate("/auth/register"); // R
+    } catch (error) {
+      // Handle login errors here
+      <Link to="/auth/register"/>
+      console.error(error);
+    }
+  };
   return (
     <>
     <h1 className="text-4xl font-bold">Inicia sesión</h1>
     <p>Para entrar debes inciar sesión</p>
 
     <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-      <form>
-
-
-        <div className="mb-4">
-            <label 
-            className="text-slate-800 mr-5"
-            htmlFor="nombre"
-            >
-
-            Email
-            </label>
-            <input type ="email"
-            id="email"
-            className="mt-2 w-full p-3 bg-gray-50"
-            name="email"
-            placeholder="Escribe tu email"
-            />
-          
-
-        
-        </div>
-
-        <div className="mb-4">
-            <label 
-            className="text-slate-800 mr-5"
-            htmlFor="nombre"
-            >
-
-            Contraseña
-            </label>
-            <input type ="password"
-            id="password"
-            className="mt-2 w-full p-3 bg-gray-50"
-            name="password"
-            placeholder="Escribe tu email"
-            />
-          
-
-        
-        </div>
-
-        <input
+    <form onSubmit={handleLogin}>
+      {/* ... */}
+      <input
+        type="email"
+        id="email"
+        className="mt-2 w-full p-3 bg-gray-50"
+        name="email"
+        placeholder="Escribe tu email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {/* ... */}
+      <input
+        type="password"
+        id="password"
+        className="mt-2 w-full p-3 bg-gray-50"
+        name="password"
+        placeholder="Escribe tu contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {/* ... */}
+      <input
         type="submit"
         value="Iniciar sesion"
         className="bg-red-500 hover:bg-red-600 w-full p-3 text-white uppercase font-bold cursor-pointer"
-        />
-
-
-      </form>
+      
+      />
+      {/* ... */}
+    </form>
     </div>
     <nav className="mt-10 flex justify-center">
 
