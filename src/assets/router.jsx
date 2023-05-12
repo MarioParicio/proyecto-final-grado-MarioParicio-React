fd52a22754e17414adee1763934bb95fce5d85ae
Mario <mpariciob@gmail.com>
2023-05-12 15:20:13 +0200

    Prueba

--- src/assets/router.jsx
+++ src/assets/router.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/',
-        element: <Navigate to="/inicio" replace />,
+        element: <Navigate to="/inicio" replace/>,
       },
     
     ],
