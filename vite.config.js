a0f61cc48c5df76c24b8f74b9d82346728347e13
Mario <mpariciob@gmail.com>
2023-05-12 14:35:03 +0200

    :)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@/components/Resumen.jsx'],
+  resolve: {
+    alias: {
+      '@components': '/src/components', // Ejemplo de alias para la carpeta de componentes
+      '@assets': '/src/assets', // Ejemplo de alias para la carpeta de assets
+      // Agrega más alias según tus necesidades
     },
   },
 });
