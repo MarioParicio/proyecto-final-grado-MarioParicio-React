999a49fa01f4d9342409e77c00cd3d5714ad5dfc
Mario <mpariciob@gmail.com>
2023-05-31 21:17:40 +0200

    Actualizada informacion a mostrar en mis pedidos :)

--- src/views/MisOrdenes.jsx
+++ src/views/MisOrdenes.jsx
@@ -12,8 +12,8 @@ export default function MisOrdenes() {
            
  
         <div className='m-5'>
-            <h1 className='text-4xl  font-bold'>Ordenes</h1>
-            <p className='text-xl my-10'>Administra las ordenes desde aquí</p>
+            <h1 className='text-4xl  font-bold'>Mis pedidos</h1>
+            <p className='text-xl my-10'>Aquí puedes ver todos los pedidos que has realizado</p>
          
 
             
@@ -31,33 +31,26 @@ export default function MisOrdenes() {
                             key={bocadillo.uid}
                             className='border-b  border-b-slate-200  last-of-type:border-nome  py-4'
                             >
-                                <p className='text-sm'>ID: {bocadillo.uid} </p> 
-                                <p className='text-base font-bold'>Nombre: {bocadillo.bocadilloName}</p> 
+            
+                                <p className='text-lg  '>Nombre: {bocadillo.bocadilloName}</p> 
                                 
-                                <p className='font-bold'>
+                                <p className='text-lg  '>
                                     Cantidad: {bocadillo.cantidad}
 
                                 </p>
-                                <p>
+                                
+                                <p className='text-lg '>
+                                    Fecha: {order.dateOrder}
+                                </p>
+                                <p className='text-lg  '>
                                     Nota: {bocadillo.nota}
                                 </p>
                             </div> 
                         )) 
                         }
-                        <p className='text-lg font-bold text-amber-600'>
-                            Cliente: {''}
-                            <span className='font-normal '>{order.nameClient}
-
-                            </span>
-
-                        </p>
-                        <p className='text-lg font-bold text-amber-600'>
-
-                            Email: {''}
-                            <span className='font-normal '>{order.email}
-                            </span>
-                        </p>
-                        <p className='text-lg font-bold text-amber-600'>
+                      
+                       
+                        <p className='text-lg font-bold '>
                             Total a pagar: {''}
                            <span className='font-normal text-slate-600'>{formatearDinero( order.total)}
 
